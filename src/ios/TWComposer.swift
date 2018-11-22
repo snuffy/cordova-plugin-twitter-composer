0049885c87800d44be0114e6fae18e2fff371039
naka <snuffy.tokyo@gmail.com>
2018-11-22 11:38:32 +0900

    error  handling and send cordova status

--- src/ios/TWComposer.swift
+++ src/ios/TWComposer.swift
@@ -13,43 +13,63 @@ import AlamofireImage
     }
 
     func compose(_ command: CDVInvokedUrlCommand) {
+        
         let composer = TWTRComposer()
-        let text = command.argument(at: 0)
+        let text = command.argument(at: 0) as! String
         let url = command.argument(at: 1)
         
-        // set text
-        composer.setText(text as! String);
+        // setting text
+        composer.setText(text);
         
+        // init returning message to clients
+        var returnMessage:[String: Any] = [:]
         
         // set image
         if let ur = url {
             let targetURL = URL(string:ur as! String)
             let imageView = UIImageView()
             imageView.contentMode = UIViewContentMode.scaleAspectFit;
+            
+            // gettig image from URL, so this action is async
             imageView.af_setImage(withURL: targetURL!) { response in
                 if let error = response.result.error {
-                    // TODO: error handling
-                    print(error)
+                    returnMessage["status"] = "failed"
+                    returnMessage["title"] = "failed getting image from url"
+                    returnMessage["description"] = error.localizedDescription
+                    let result = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: returnMessage)
+                    self.commandDelegate.send(result, callbackId: command.callbackId)
                 }
                 else {
                     composer.setImage(response.result.value)
                     composer.show(from: self.viewController!) { result in
+                        
                         if (result == .done) {
-                            print("Success dfully composed Tweet")
+                            returnMessage["status"] = "success"
+                            returnMessage["title"] = "send composing tweet with image"
                         } else {
-                            print("Cancelled composing")
+                            returnMessage["status"] = "cancel"
+                            returnMessage["title"] = "cancel composing tweet"
                         }
+                        
+                        let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: returnMessage)
+                        self.commandDelegate.send(result, callbackId: command.callbackId)
                     }
                 }
             }
         }
         else {
             composer.show(from: self.viewController!) { result in
+                
                 if (result == .done) {
-                    print("Success dfully composed Tweet")
+                    returnMessage["status"] = "success"
+                    returnMessage["title"] = "send composing tweet "
                 } else {
-                    print("Cancelled composing")
+                    returnMessage["status"] = "cancel"
+                    returnMessage["title"] = "cancel composing tweet"
                 }
+                
+                let result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: returnMessage)
+                self.commandDelegate.send(result, callbackId: command.callbackId)
             }
         }
     }
