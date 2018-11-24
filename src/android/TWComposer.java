ba86910f902c0b98f1c29d4a146ab66899a59277
naka <snuffy.tokyo@gmail.com>
2018-11-24 10:05:35 +0900

    base of android

---
+++ src/android/TWComposer.java
@@ -0,0 +1,41 @@
+package jp.snuffy;
+
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.TwitterAuthClient;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.internal.TwitterApi;
+import com.twitter.sdk.android.core.models.*;
+import com.twitter.sdk.android.core.services.*;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaWebView;
+import org.apache.cordova.CallbackContext;
+
+import jdk.nashorn.api.scripting.JSObject;
+import jdk.nashorn.internal.runtime.JSONListAdapter;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+
+public class TWComposer extends CordovaPlugin {
+
+}
